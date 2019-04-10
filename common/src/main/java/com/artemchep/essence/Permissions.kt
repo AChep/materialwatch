4fb91f9b3ed433b5ef4f417b8cc0d8ac051d7a30
Artem Chepurnoy <artemchep@gmail.com>
2019-04-10 16:02:09 +0300

    Add ACCESS_FINE_LOCATION permission

--- common/src/main/java/com/artemchep/essence/Permissions.kt
+++ common/src/main/java/com/artemchep/essence/Permissions.kt
@@ -3,5 +3,6 @@ package com.artemchep.essence
 import android.Manifest
 
 val RUNTIME_PERMISSIONS = listOf(
-    Manifest.permission.ACCESS_COARSE_LOCATION
+    Manifest.permission.ACCESS_COARSE_LOCATION,
+    Manifest.permission.ACCESS_FINE_LOCATION
 )
